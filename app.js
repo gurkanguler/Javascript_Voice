5ab19fb3140f2d4f955af7ad5c20e3a86b9e9311
Gürkan Güler <coder.gurkanguler16@gmail.com>
2020-10-09 16:31:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const recognition = new SpeechRecognition();
 
 recognition.onstart = function(){
-    console.log('You can speak now!!!');
+    console.log('Şimdi Konuşabilirsin!!');
 }
 
 recognition.onresult = function(event){
@@ -18,8 +18,8 @@ function read(text){
     var speech = new SpeechSynthesisUtterance();
     speech.text = text;
     if(text.includes('time'))
-    speech.text = 'It is '+new Date().getHours() + " " + new Date().getMinutes()+" right now";
-    else if(text.includes('my birthday'))
+    speech.text = 'It is '+new Date().getHours() + " " + new Date().getMinutes()+" şimdi";
+    else if(text.includes('dogum gunum'))
     speech.text = 'Do you think you\'re famous! How the heck would I know your birthday!';
     else if(text.includes('love me'))
     speech.text = 'Of course, not! You piece of junk!';
